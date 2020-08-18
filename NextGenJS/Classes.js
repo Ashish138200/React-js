6400f9110822c017c71d1b577b876d1c663c1a63
Ashish Chaurasia <ashish.chaurasia_ccv18@gla.ac.in>
2020-08-18 11:46:27 +0530

    Classes

---
+++ NextGenJS/Classes.js
@@ -0,0 +1,21 @@
+class Human{
+    constructor() {
+        this.gender = 'Male'
+    }
+    myGender(){
+        console.log(this.gender)
+    }
+}
+
+class Person extends Human{
+    constructor() {
+        super();
+        this.name = 'Ashish'
+    }
+    myName(){
+        console.log(this.name);
+    }
+}
+const person = new Person()
+person.myName()
+person.myGender()
