557944664141e308fbea01f62055964ba80ce1a8
Ashish Chaurasia <ashish.chaurasia_ccv18@gla.ac.in>
2020-08-18 11:47:11 +0530

    Variables

---
+++ NextGenJS/demoOfVar.js
@@ -0,0 +1,8 @@
+//let is just like var for a variable value
+// const is for a constant value
+let myName = 'Ashish';
+var y  = 'Ashish';
+const z = 'Ashish';
+console.log(myName)
+console.log(y)
+console.log(z)
