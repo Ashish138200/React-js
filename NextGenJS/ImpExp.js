434331c7927ac5911848754364a13e2d7a9282e0
Ashish Chaurasia <ashish.chaurasia_ccv18@gla.ac.in>
2020-08-18 11:46:53 +0530

    Import and Exports

---
+++ NextGenJS/ImpExp.js
@@ -0,0 +1,9 @@
+/*
+* DEFAULT EXPORT
+ import person from './person.js'
+ import prsn from './person.js'
+* NAMED EXPORT
+ import {smthg} from './utility.js'
+ import {smthg as something} from './utility.js' // giving the alias to the object
+ import * as bundled from './utility.js'  // for importing everything
+*/
